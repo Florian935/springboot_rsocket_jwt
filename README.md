211107c986f2f471ab04911d80e49669bc6eef9f
florian935 <florian.martin63000@gmail.com>
2021-08-18 18:01:25 +0200

    Feat: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Boot project with RSocket JWT
+
+The purpose of this project is to demonstrate how to secure a RSocket application with a JWT in the metadata send by a requester.
---
+++ requester/src/main/java/com/florian935/requester/rsocketjwt/Application.java
@@ -0,0 +1,13 @@
+package com.florian935.requester.rsocketjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
